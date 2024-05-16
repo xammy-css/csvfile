d535632024b4c328a4b1b989a910c05d194f9b5a
Max Vasyschev <maxvas@gmail.com>
2024-05-16 10:59:57 +0300

    Find high price

--- csvfile.py
+++ csvfile.py
@@ -2,8 +2,16 @@
 with open("tarifas.csv", "r", encoding="ISO-8859-1") as file:
     data = file.readlines()
     data.pop(0)
+    lowprice = data[0].split(";")
     for item in data:
         row_data = item.split(";")
+        if (row_data[-1].strip()) == "Precio pvd globomatik":
+            continue
         if len(row_data) < 2:
             continue
-        print(row_data[0], row_data[1], row_data[-1].strip())
+        try:
+            if float(row_data[-1].strip().replace("," , ".")) > float(lowprice[-1].strip().replace("," , ".")):
+                lowprice = row_data
+        except ValueError:
+            pass
+    print(lowprice[0], lowprice[1], lowprice[-1].strip())
